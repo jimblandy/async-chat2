7afe48e318360ff7ec8db97349fab8abca08d42f
Jim Blandy <jimb@red-bean.com>
2020-05-17 18:10:53 -0700

    Define `receive_as_json`.

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,8 @@
 //! Utilities for both clients and servers.
 
-use async_std::net;
+use async_std::{io, net};
 use async_std::prelude::*;
-use serde::Serialize;
+use serde::{de::DeserializeOwned, ser::Serialize};
 use std::error::Error;
 
 /// Our crate's `Error` and `Result` type, designed for flexibility.
@@ -16,6 +16,17 @@ pub async fn send_as_json<V: Serialize>(socket: &mut net::TcpStream, value: &V) 
     json.push('\n');
     socket.write_all(json.as_bytes()).await?;
     Ok(())
+}
+
+/// Read lines from `stream`, parse them as the JSON-serialized form of `V`
+/// values, and return a `Stream` of `ChatResult<V>` values.
+pub fn receive_as_json<V: DeserializeOwned>(socket: net::TcpStream) -> impl Stream<Item=ChatResult<V>> {
+    let buffered = io::BufReader::new(socket);
+    buffered.lines().map(|result| {
+        result
+            .map_err(|e| ChatError::from(e))
+            .and_then(|line| serde_json::from_str::<V>(&line).map_err(Into::into))
+    })
 }
 
 /// Await `future`, and log any error it returns.
